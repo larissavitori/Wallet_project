adeabfe5cfeb504e6a6680aa07214fa6d12b6ce9
larissa <laryterra@gmail.com>
2023-02-01 19:59:40 -0300

    02

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Hearder from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email, total = 0, moeda = 'BRL' } = this.props;
+    return (
+      <section>
+        <div>TrybeWallet</div>
+        <Hearder />
+        <div data-testid="email-field">
+          email:
+          { email }
+        </div>
+        <div data-testid="total-field">
+          despesa tota:
+          { total }
+        </div>
+        <div data-testid="header-currency-field">
+          Moeda:
+          { moeda }
+        </div>
+      </section>
+    );
   }
 }
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+  total: PropTypes.number.isRequired,
+  moeda: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
